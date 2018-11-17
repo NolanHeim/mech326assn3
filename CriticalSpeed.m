6df718e16e70bdee1d08dae27b9f7c68f91ce8be
Nolan Heim <nheim@alumni.ubc.ca>
2018-11-17 14:12:06 -0800

    deflection matrix changed to function

--- CriticalSpeed.m
+++ CriticalSpeed.m
@@ -1,24 +1,22 @@
-%%
+%% Determines the Critical Speed of the Shaft
 % Inputs:
-% yji - matrix of deflections
-%      -> each row is the deflections for each slice in the section
+% y - Function of xi for the deflections.
 % rho - density of material
 % xj - section boundary points
-% dj - diameters between xi's
+% dj - diameters between xj's
 % Ni - Number of elements for each section (j) (integer)
 
-function omega1 = CriticalSpeed(yji, rho, xj, dj, Ni)
+function omega1 = CriticalSpeed(y, rho, xj, dj, Ni)
     g = 9.81; %Gravitational Constant
     sum_wjyj = 0;
     for j = 1:5
         d = dj(j);
-        ti = linspace(xj(j), xj(j+1), Ni);
-        mi = (rho*pi*(d^2)*ti)/4;
+        xi = linspace(xj(j), xj(j+1), Ni);
+        mi = (rho*pi*(d^2)*xi)/4;
         wi = g*mi;
-        yi = yji(j,:);
+        yi = y(xi);
         sum_wjyj = sum_wjyj + sum(yi.*wi); 
     end
     omega1 = sqrt((g*sum_wjyj)/(sum_wjyj));
-
 end
 
